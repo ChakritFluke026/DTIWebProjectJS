407a98c2d4c82a3631366b0e12623033fdaa8015
ChakritFluke026 <s6752410026@sau.ac.th>
2025-03-25 15:59:56 +0700

    Add files via upload

---
+++ DTIWebProjectJS/js02/study05.js
@@ -0,0 +1,24 @@
+//Operator
+//Arithmetic Operators + - * / % ** ++ --
+//****Comparison Operators == === != !== > < >= <=
+console.log('10' == 10); //true
+console.log('10' === 10); //false
+
+//Logical Operators ! && ||
+
+//assignment Operators = += -= *= /= %= **=
+x = x + 1 + 554 * y
+x += 1 + 554 * y
+
+//Ternary Operator  ____? ____:____
+let aa = 10
+let bb = aa - 3 * 2 === '25' ? 'Wow...' : 'Hi...'
+
+console.log(bb)
+
+//Nullish Coalescing Operator ??
+let cc = 50
+let dd = null
+console.log(cc ?? 'SAU...') //50
+console.log(dd ?? 'DTI...') //DTI...
+
